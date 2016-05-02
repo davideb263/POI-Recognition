2a24a7df09f6f6b027888723f9f70440a1b96698
Valentina Sanguineti <valentina.sanguineti@fastwebnet.it>
2016-05-02 11:27:47 +0200

    prima parte

---
+++ POIrecognition/src/com/example/poirecognition/POI_recognition.java
@@ -0,0 +1,33 @@
+package com.example.poirecognition;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class POI_recognition extends Activity{
+private TextView poiSL=null;
+private Button bttback=null;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.poi_recognition);
+		poiSL=(TextView)findViewById(R.id.poisl);
+		bttback=(Button)findViewById(R.id.backToMain);
+		Intent recognitionIntent =getIntent();
+		String s=recognitionIntent.getStringExtra("stringStart");
+		bttback.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				setResult(Activity.RESULT_OK, new Intent().putExtra("recognition", "rec"));
+				finish();
+			}
+		});
+		
+	}
+}
